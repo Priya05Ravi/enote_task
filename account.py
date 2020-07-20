24f3df141bbbfcd28674db9b12dc498985bc28f1
Priya05Ravi <68516116+Priya05Ravi@users.noreply.github.com>
2020-07-20 09:00:05 +0200

    Update account.py

--- account.py
+++ account.py
@@ -9,7 +9,7 @@ sql_conn = pyodbc.connect("Driver={SQL Server Native Client 11.0};"
 df = pd.read_csv("BI_assignment_account.csv", delimiter = ',')
 df2 = df.fillna(value=0)
 cursor = sql_conn.cursor()
-cursor.execute("CREATE TABLE enote_task.ACCOUNT(id_account VARCHAR(20) PRIMARY KEY,id_person VARCHAR(20) FOREIGN KEY REFERENCES enote_task.CUSTOMERS11(id_person),account_type VARCHAR(20))")
+cursor.execute("CREATE TABLE enote_task.ACCOUNT(id_account VARCHAR(20) PRIMARY KEY,id_person VARCHAR(20) FOREIGN KEY REFERENCES enote_task.CUSTOMERS(id_person),account_type VARCHAR(20))")
 for index, row in df2.iterrows():
    #print(row)
  cursor.execute("INSERT INTO enote_task.ACCOUNT([id_account],[id_person],[account_type]) values(?,?,?)",  row['id_account'], row['id_person'], row['account_type'])
