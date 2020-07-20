4781dfd823e9fca43b5f3f080b6c951a7f3adb43
Priya05Ravi <68516116+Priya05Ravi@users.noreply.github.com>
2020-07-20 09:32:27 +0200

    Update result.py

--- result.py
+++ result.py
@@ -6,7 +6,7 @@ sql_conn = pyodbc.connect("Driver={SQL Server Native Client 11.0};"
                       "Database=master;"
                       "Trusted_Connection=yes;")
 cursor = sql_conn.cursor()
-cursor.execute("CREATE VIEW  Employee ([amt], [date] ,[account]) AS select sum(transaction_amount),(FORMAT(transaction_date, 'MM/yy')),id_account as transDate from enote_task.TRANSACTION where id_account in (345,1234) group by DATEPART(month, transaction_date),id_account,transaction_date")
+cursor.execute("CREATE VIEW  Employee ([amt], [date] ,[account]) AS select sum(transaction_amount),(FORMAT(transaction_date, 'MM/yy')),id_account as transDate from enote_task.TRANSACTION568 where id_account in (345,1234) group by DATEPART(month, transaction_date),id_account,transaction_date")
 sql_command="""select account,date as month,sum(amt) as sum_transactions from Employee group by date,account""";
 cursor.execute(sql_command);
 result = cursor.fetchall() 
