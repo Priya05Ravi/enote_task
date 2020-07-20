1976bcdba10408a5046fa2e08bbcdf8228456f45
Priya05Ravi <68516116+Priya05Ravi@users.noreply.github.com>
2020-07-20 09:31:30 +0200

    Update transaction.py

--- transaction.py
+++ transaction.py
@@ -10,10 +10,10 @@ df = pd.read_csv("BI_assignment_transaction.csv", delimiter = ',')
 df2 = df.fillna(value=0)
 cursor = sql_conn.cursor()
 
-cursor.execute("CREATE TABLE enote_task.TRANSACTION(id_transaction VARCHAR(10),id_account VARCHAR(20) FOREIGN KEY REFERENCES ACCOUNT(id_account),transaction_type VARCHAR(20),transaction_date DATE,transaction_amount float(20))")
+cursor.execute("CREATE TABLE enote_task.TRANSACTION568(id_transaction VARCHAR(10),id_account VARCHAR(20) FOREIGN KEY REFERENCES ACCOUNT1(id_account),transaction_type VARCHAR(20),transaction_date DATE,transaction_amount float(20))")
 for index, row in df2.iterrows():
    #print(row)
- cursor.execute("INSERT INTO enote_task.TRANSACTION([id_transaction],[id_account],[transaction_type],[transaction_date],[transaction_amount]) values(?,?,?,?,?)", row['id_transaction'], row['id_account'], row['transaction_type'], row['transaction_date'],row['transaction_amount'])
+ cursor.execute("INSERT INTO enote_task.TRANSACTION568([id_transaction],[id_account],[transaction_type],[transaction_date],[transaction_amount]) values(?,?,?,?,?)", row['id_transaction'], row['id_account'], row['transaction_type'], row['transaction_date'],row['transaction_amount'])
  
  sql_conn.commit()
 cursor.close()
