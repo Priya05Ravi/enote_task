0d34b14c9907d56f407aa73aa618549e365450c3
Priya05Ravi <68516116+Priya05Ravi@users.noreply.github.com>
2020-07-20 09:00:50 +0200

    Update person.py

--- person.py
+++ person.py
@@ -11,11 +11,11 @@ df = pd.read_csv("BI_assignment_person.csv", delimiter = ',',nrows=19947)
 df2 = df.fillna(value="")
 cursor = sql_conn.cursor()
 cursor.execute("CREATE SCHEMA enote_task")
-cursor.execute("CREATE TABLE enote_task.CUSTOMERS11(id_person VARCHAR(20) NOT NULL,name VARCHAR(200),surname VARCHAR(250),zip VARCHAR(50),city VARCHAR(100) ,country  VARCHAR(250) ,email  VARCHAR(250),phone_number  VARCHAR(25),birth_date VARCHAR(20),PRIMARY KEY (id_person))")
+cursor.execute("CREATE TABLE enote_task.CUSTOMERS(id_person VARCHAR(20) NOT NULL,name VARCHAR(200),surname VARCHAR(250),zip VARCHAR(50),city VARCHAR(100) ,country  VARCHAR(250) ,email  VARCHAR(250),phone_number  VARCHAR(25),birth_date VARCHAR(20),PRIMARY KEY (id_person))")
 for index, row in df2.iterrows():
    #print(row)
  #if not df2.empty:
-    cursor.execute("INSERT INTO enote_task.CUSTOMERS11([id_person],[name],[surname],[zip],[city],[country],[email],[phone_number],[birth_date]) values(?,?,?,?,?,?,?,?,?)", row['id_person'], row['name'], row['surname'], row['zip'],row['city'], row['country'], row['email'], row['phone_number'], row['birth_date'])
+    cursor.execute("INSERT INTO enote_task.CUSTOMERS([id_person],[name],[surname],[zip],[city],[country],[email],[phone_number],[birth_date]) values(?,?,?,?,?,?,?,?,?)", row['id_person'], row['name'], row['surname'], row['zip'],row['city'], row['country'], row['email'], row['phone_number'], row['birth_date'])
  
     sql_conn.commit()
 cursor.close()
